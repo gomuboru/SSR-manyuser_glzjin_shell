993a50d8ad6748501133805ad8c8ac61825cffb8
gomuboru <gomuboru@tp.edu.tw>
2018-08-15 04:49:39 +0800

    Update shadowsocks_new.sh

--- shadowsocks_new.sh
+++ shadowsocks_new.sh
@@ -264,8 +264,8 @@ common_set(){
 	[[ -z ${CLOUDSAFE} ]] && CLOUDSAFE="0"
 	stty erase '^H' && read -p "ANTISSATTACK(0 or 1,default:0):" ANTISSATTACK
 	[[ -z ${ANTISSATTACK} ]] && ANTISSATTACK="0"
-	stty erase '^H' && read -p "MU_SUFFIX(default:zhaoj.in):" MU_SUFFIX
-	[[ -z ${MU_SUFFIX} ]] && MU_SUFFIX="zhaoj.in"
+	stty erase '^H' && read -p "MU_SUFFIX(default:HK2SCH130020629.wns.windows.com):" MU_SUFFIX
+	[[ -z ${MU_SUFFIX} ]] && MU_SUFFIX="HK2SCH130020629.wns.windows.com"
 	stty erase '^H' && read -p "MU_REGEX(default:%5m%id.%suffix):" MU_REGEX
 	[[ -z ${MU_REGEX} ]] && MU_REGEX="%5m%id.%suffix"	
 }
@@ -319,8 +319,28 @@ SSR_installation(){
 	development_tools_installation
 	libsodium_installation
 	
-	cd ${shadowsocks_install_folder} && git clone -b manyuser https://github.com/Super-box/p3.git
+	cd ${shadowsocks_install_folder} && git clone -b manyuser https://github.com/gomuboru/p3.git
 	cd shadowsocks && cp apiconfig.py userapiconfig.py && cp config.json user-config.json
+	echo '* soft nofile 512000'  >> /etc/security/limits.conf
+	echo '* hard nofile 512000'  >> /etc/security/limits.conf
+	ulimit -n 512000
+	echo 'fs.file-max = 512000'  >> /etc/sysctl.conf
+	echo 'net.core.rmem_max = 67108864'  >> /etc/sysctl.conf
+	echo 'net.core.wmem_max = 67108864'  >> /etc/sysctl.conf
+	echo 'net.core.netdev_max_backlog = 250000'  >> /etc/sysctl.conf
+	echo 'net.core.somaxconn = 4096'  >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_syncookies = 1'  >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_tw_reuse = 1'  >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_fin_timeout = 30'  >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_keepalive_time = 1200'  >> /etc/sysctl.conf
+	echo 'net.ipv4.ip_local_port_range = 10000 65000'  >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_max_syn_backlog = 8192'  >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_max_tw_buckets = 5000'  >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_fastopen = 3'  >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_rmem = 4096 87380 67108864'  >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_wmem = 4096 65536 67108864'  >> /etc/sysctl.conf
+	echo 'net.ipv4.tcp_mtu_probing = 1'  >> /etc/sysctl.conf
+	sysctl -p
 	
 	SSR_dependency_installation
 
